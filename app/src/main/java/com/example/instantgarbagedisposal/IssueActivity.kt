824067653e8eda0993821287b77d66e170443a6b
Palash <palashwalali25@gmail.com>
2023-01-21 22:17:21 +0530

    Added some imagebutton icons and shit

--- app/src/main/java/com/example/instantgarbagedisposal/IssueActivity.kt
+++ app/src/main/java/com/example/instantgarbagedisposal/IssueActivity.kt
@@ -2,10 +2,26 @@ package com.example.instantgarbagedisposal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.Toast
 
 class IssueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_issue)
+
+        val garbage_type = resources.getStringArray(R.array.garbage_type)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, garbage_type)
+        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        autocompleteTV.setAdapter(arrayAdapter)
+
+        val postButton: Button = findViewById(R.id.post)
+
+        postButton.setOnClickListener {
+            finish()
+            Toast.makeText(this, "Posted!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/instantgarbagedisposal/MainActivity.kt
+++ app/src/main/java/com/example/instantgarbagedisposal/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.instantgarbagedisposal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val issueButton : Button = findViewById(R.id.issueButton)
+
+        issueButton.setOnClickListener {
+            val intent: Intent = Intent(
+                this,
+                IssueActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+
+
     }
 }
