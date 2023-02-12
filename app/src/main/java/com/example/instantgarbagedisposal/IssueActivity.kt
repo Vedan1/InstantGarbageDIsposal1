f689d68478adc838125d376c461d5636a5b326ca
Palash <palashwalali25@gmail.com>
2023-02-12 16:13:52 +0530

    Added the ability to add images and shit Next: Location addition

--- app/src/main/java/com/example/instantgarbagedisposal/IssueActivity.kt
+++ app/src/main/java/com/example/instantgarbagedisposal/IssueActivity.kt
@@ -1,13 +1,26 @@
 package com.example.instantgarbagedisposal
 
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.Toast
+import android.provider.MediaStore
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class IssueActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var takePicture : ImageButton
+    private val REQUEST_CODE = 100
+    private val REQUEST_GALLERY_CAMERA = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_issue)
@@ -17,11 +30,54 @@ class IssueActivity : AppCompatActivity() {
         val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
         autocompleteTV.setAdapter(arrayAdapter)
 
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE)
+
+        imageView = findViewById(R.id.imageView)
+        takePicture = findViewById(R.id.btnTakePicture)
+
+
+        takePicture.setOnClickListener{
+            val cameraIntent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (cameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(cameraIntent, REQUEST_CODE)
+            }
+        }
+
+
         val postButton: Button = findViewById(R.id.post)
+
+
 
         postButton.setOnClickListener {
             finish()
             Toast.makeText(this, "Posted!",Toast.LENGTH_SHORT).show()
         }
     }
+
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == REQUEST_GALLERY_CAMERA) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+//                openCamera()
+//            } else {
+//                Toast.makeText(this@CameraIntentActivity, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            val imageBitmap: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+
+        }
+
+
+
+    }
+
+
 }
