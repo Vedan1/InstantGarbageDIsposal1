5658cb1440be1564d820bc4787f80fcd39297e2c
vedan1 <vedantpingale1@gmail.com>
2023-03-30 23:36:03 +0530

    Message Section Added to Supervisor And Firebase. Minor changes to Setting Profile

--- app/src/main/java/com/example/instantgarbagedisposal/AdminActivity.kt
+++ app/src/main/java/com/example/instantgarbagedisposal/AdminActivity.kt
@@ -1,12 +1,32 @@
 package com.example.instantgarbagedisposal
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.instantgarbagedisposal.R
+import android.widget.Button
 
 class AdminActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        val btn1 = findViewById<Button>(R.id.noticegen)
+
+        btn1.setOnClickListener(){
+            val i = Intent(this,msgadmin::class.java)
+            startActivity(i)
+        }
+
+
+
     }
+
+
+
+
+
+
+
 }
