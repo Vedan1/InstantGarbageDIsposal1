3f9dda1c5ad687a2d1ae2628eb3c8fdfa6bd3465
vedan1 <vedantpingale1@gmail.com>
2023-04-16 16:35:45 +0530

    uploads location to the db in cordinates(issue activity_issue.xml)

--- app/src/main/java/com/example/instantgarbagedisposal/IssueActivity.kt
+++ app/src/main/java/com/example/instantgarbagedisposal/IssueActivity.kt
@@ -4,25 +4,68 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.instantgarbagedisposal.storage.Issue
 import com.example.instantgarbagedisposal.storage.Persistance
 import com.example.instantgarbagedisposal.utility.ImageViewToBase64
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.*
 
 class IssueActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private lateinit var takePicture : ImageButton
     private val requestCode = 100
-
+    var locs = 0
+    var locationManager: LocationManager? = null
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+//    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_issue)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), 101
+                )
+            }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        val curloc = fusedLocationProviderClient.lastLocation
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        val locshow = findViewById<TextView>(R.id.locshow)
+
+
+        val getloc = findViewById<Button>(R.id.getloc)
+
+
+            curloc.addOnSuccessListener{
+                    if(it != null){
+                       locshow.text = "${it.latitude} ${it.longitude}"
+                        //Toast.makeText(this,locs,Toast.LENGTH_SHORT)
+                    }
+
+            }
+
+
 
         val garbageType = resources.getStringArray(R.array.garbage_type)
         val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, garbageType)
@@ -42,14 +85,16 @@ class IssueActivity : AppCompatActivity() {
         val postButton: Button = findViewById(R.id.post)
 
         postButton.setOnClickListener {
-            val location = findViewById<EditText>(R.id.editTextTextPostalAddress2)
+            val location = findViewById<TextView>(R.id.editTextTextPostalAddress2)
             val locationValue = location.text.toString()
+            val latlong = locshow.text.toString()
             val garbageTypeValue = autocompleteTV.text.toString()
             val base64Image = ImageViewToBase64.get(imageView)
-            val newIssue: Persistance = Issue(locationValue, garbageTypeValue, base64Image)
+            val newIssue: Persistance = Issue(locationValue,latlong,garbageTypeValue,base64Image)
             newIssue.save()
             finish()
             Toast.makeText(this, "Posted!", Toast.LENGTH_SHORT).show()
+
         }
     }
 
@@ -73,6 +118,43 @@ class IssueActivity : AppCompatActivity() {
 //        }
 //    }
 
+    private fun locationEnabled() {
+        val lm = getSystemService(LOCATION_SERVICE) as LocationManager
+        var gps_enabled = false
+        var network_enabled = false
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        if (!gps_enabled && !network_enabled) {
+            AlertDialog.Builder(this@IssueActivity)
+                .setTitle("Enable GPS Service")
+                .setMessage("We need your GPS location to show Near Places around you.")
+                .setCancelable(false)
+                .setPositiveButton(
+                    "Enable"
+                ) { paramDialogInterface, paramInt -> startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)) }
+                .setNegativeButton("Cancel", null)
+                .show()
+        }
+    }
+//    fun getLocation() {
+//        try {
+//            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+//            locationManager!!.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5f, onLocationChanged())
+//        } catch (e: SecurityException) {
+//            e.printStackTrace()
+//        }
+//    }
+
+//
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -82,6 +164,11 @@ class IssueActivity : AppCompatActivity() {
 
         }
     }
+    fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+
+    fun onProviderEnabled(provider: String?) {}
+
+    fun onProviderDisabled(provider: String?) {}
 
 
 }
--- app/src/main/java/com/example/instantgarbagedisposal/storage/Issue.java
+++ app/src/main/java/com/example/instantgarbagedisposal/storage/Issue.java
@@ -6,15 +6,18 @@ import java.util.Map;
 public class Issue extends Database implements Persistance {
 
     private static final String LOCATION_TAG = "location";
+    private static final String LATLONG_TAG = "coordinates";
     private static final String GARBAGE_TYPE_TAG = "garbage_type";
     private static final String IMAGE_TAG = "image";
     public final String location;
     public final String garbageType;
     public final String image;
+    private final String latlong;
 
 
-    public Issue(String location, String garbageType, String image) {
+    public Issue(String location,String latlong, String garbageType, String image) {
         this.location = location;
+        this.latlong = latlong;
         this.garbageType = garbageType;
         this.image = image;
     }
@@ -23,6 +26,7 @@ public class Issue extends Database implements Persistance {
     public void save() {
         Map<String, Object> data = new HashMap<>();
         data.put(LOCATION_TAG, location);
+        data.put(LATLONG_TAG,latlong);
         data.put(GARBAGE_TYPE_TAG, garbageType);
         data.put(IMAGE_TAG, image);
 
